f2d1dc4a17e44ad8120dd3294e1beb91b19558ec
evgen <s4mararunner@mail.ru>
2024-12-16 17:06:42 +0300

    feat: model player and animations imported

--- SemestProject/Assets/_Source/PlayerSystem/PlayerMovement.cs
+++ SemestProject/Assets/_Source/PlayerSystem/PlayerMovement.cs
@@ -7,10 +7,21 @@ namespace PlayerSystem
     {
         public Player player;
         private Vector2 _move;
+        private Animator _animator;
+        private static readonly int IsMovingLeft = Animator.StringToHash("isMovingLeft");
+        private static readonly int IsMovingForward = Animator.StringToHash("isMovingForward");
+        private static readonly int IsMovingBackward = Animator.StringToHash("isMovingBackward");
+        private static readonly int IsMovingRight = Animator.StringToHash("isMovingRight");
 
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+        
         private void Update()
         {
             MovePlayer();
+            UpdateAnimation();
         }
         
         public void OnMove(InputAction.CallbackContext context)
@@ -22,6 +33,40 @@ namespace PlayerSystem
         {
             Vector3 movement = new Vector3(_move.x, 0f, _move.y);
             transform.Translate(movement * (player.speed * Time.deltaTime), Space.World);
+        }
+        
+        private void UpdateAnimation()
+        {
+            _animator.SetBool(IsMovingForward, false);
+            _animator.SetBool(IsMovingBackward, false);
+            _animator.SetBool(IsMovingLeft, false);
+            _animator.SetBool(IsMovingRight, false);
+
+            if (_move.y > 0.1f)
+            {
+                _animator.SetBool(IsMovingForward, true);
+            }
+            else if (_move.y < -0.1f)
+            {
+                _animator.SetBool(IsMovingBackward, true);
+            }
+
+            if (_move.x < -0.1f)
+            {
+                _animator.SetBool(IsMovingLeft, true);
+            }
+            else if (_move.x > 0.1f)
+            {
+                _animator.SetBool(IsMovingRight, true);
+            }
+            
+            if (_move.magnitude < 0.1f)
+            {
+                _animator.SetBool(IsMovingForward, false);
+                _animator.SetBool(IsMovingBackward, false);
+                _animator.SetBool(IsMovingLeft, false);
+                _animator.SetBool(IsMovingRight, false);
+            }
         }
     }
 }
